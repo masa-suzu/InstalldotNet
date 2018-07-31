ae117b573350a334a096c3a0f5605e93d9ab963a
masa-suzu <szk.masato@icloud.com>
2018-08-01 01:05:40 +0900

    Bootstrap installer with Custom UI

---
+++ UI/CustomBootstrapperApplication.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System.Windows.Threading;
+
+namespace Install.Net.UI
+{
+
+    public class CustomBootstrapperApplication : BootstrapperApplication
+    {
+        protected override void Run()
+        {
+            var dispatcher = Dispatcher.CurrentDispatcher;
+            var w = new MainWindow();
+            w.Closed += (_, __) => dispatcher.InvokeShutdown();
+
+            w.Show();
+            Dispatcher.Run();
+            Engine.Quit(0);
+        }
+    }
+}
---
+++ UI/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Install.Net.UI
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
